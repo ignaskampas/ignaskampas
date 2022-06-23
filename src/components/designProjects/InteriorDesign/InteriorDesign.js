b5838fc22d7f78fba04b08c038f1e438bed5901c
Ignas <ikampas@yahoo.co.uk>
2022-06-23 16:24:32 +0200

    Added design project descriptions

--- src/components/designProjects/InteriorDesign/InteriorDesign.js
+++ src/components/designProjects/InteriorDesign/InteriorDesign.js
@@ -13,7 +13,7 @@ export default function InteriorDesign(){
         <div className={styles.content}>
             <div className={styles.center}>
                 <ShowcaseSection title="Interior Design Website">
-                    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</div>
+                    <div >This project consists of website UI designs for a hypothetical interior design company called DesignRise.</div>
                     <div></div>
                 </ShowcaseSection>
             </div>
--- src/components/designProjects/StudyCrew/StudyCrew.js
+++ src/components/designProjects/StudyCrew/StudyCrew.js
@@ -8,15 +8,7 @@ export default function StudyCrew() {
             <div className={styles.center}>
                 <ShowcaseSection title="Study Crew">
                     <div>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu
-                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                        non proident, sunt in culpa qui officia deserunt mollit
-                        anim id est laborum
+                    This project consists of UI designs for a hypothetical mobile app called StudyCrew. The app allows users to meet new people that they can revise online with either via a video call or audio call. 
                     </div>
                     <div></div>
                 </ShowcaseSection>
