488bd0689a8d2864a340234473b03f3acd181481
Ignas <ikampas@yahoo.co.uk>
2022-10-17 22:40:12 +0100

    Updated personal summary

--- src/components/sections/Home/Home.js
+++ src/components/sections/Home/Home.js
@@ -18,7 +18,7 @@ export default function Home() {
                         </span>
                     </div>
                 </div>
-                <div className={styles.introMessage}>A diligent and highly driven Computer Science BSc graduate seeking a challenging role as a Front End Web Developer. An individual who works effectively both in a team and independently, and is eager to expand his skillset developed in both the University and outside of the University activities.</div>
+                <div className={styles.introMessage}>A solution driven freelance Web Developer seeking a role as a Front-End Web Developer or freelance work. An individual who works effectively both in a team and independently with a proven history of high standards and passion for web development.</div>
                 <div className={styles.icons}>
 
                 </div>
